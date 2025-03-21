b4d1e856b3e08d5dbb1af2b2253545c842fd646d
Tharsoso <tharsosfernandes@gmail.com>
2025-03-21 12:57:18 -0300

    Pesquisa cidade e eventos

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main(){
     //Danilo, essa é a parte do roteiro de viagem, você precisará colocar no lugar certo
     int totalEventos;
     char eventosEscolhidos[MAX_EVENTOS][MAX_NOME]; 
+    char nomePesquisa[MAX_NOME];
     
     // Solicitar ao usuário a quantidade de eventos desejada
     do {
@@ -29,6 +30,20 @@ int main(){
         printf("Digite o nome do evento %d: ", i + 1);
         scanf(" %49[^\n]", eventosEscolhidos[i]);
     }
+   
+    
+    //pesquisas de cidade e evento dps tem colocar no local correto da main
+    printf("Digite o nome da cidade para procurar: ");
+    scanf(" %49[^\n]", nomePesquisa);
+
+    ConfirmarCidade(&arvore, nomePesquisa);
+    
+    printf("Digite o nome do evento para verificar em qual cidade ele está: ");
+    scanf(" %49[^\n]", nomePesquisa);
+
+    ConfirmarEvento(&arvore, nomePesquisa);
+
+
     encontrarMelhorRota(&arvore, eventosEscolhidos, totalEventos);
     liberarArvore(arvore.raiz);
     //FIM CAIXEIRO
